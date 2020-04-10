1f935002500672ac78a28ec192ec0c3e8b808e98
jmnmoyano25 <jmnmoyano@hotmail.com>
2020-04-10 18:51:16 +0200

    .

---
+++ EntornosDAW/src/entornoDesarroyo/ProbandoPreExamen.java
@@ -0,0 +1,22 @@
+package entornoDesarroyo;
+
+public class ProbandoPreExamen {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+		int a = 4;
+		
+		while (a > 0) {
+			if (a > 1) {
+				System.out.println("hola");
+			
+			}else {
+				System.out.println("adios");
+			}
+			a--;
+		}
+		
+	}
+
+}
